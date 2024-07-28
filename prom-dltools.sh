a5ebd5eb630ec4015cb2ecfbf312176be15f3c98
dani <skyline@skyline>
2024-07-28 23:51:47 +0330

    v0.8

---
+++ prom-dltools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##put some variables with version, you should change if the newer version released
+NODE_EXPORTER="https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.darwin-amd64.tar.gz"
+
+urls=(
+	"$NODE_EXPORTER"
+)
+
+for url in "${urls[@]}"; do
+	wget -p ./roles/promtools/files/ "$url"
+done
